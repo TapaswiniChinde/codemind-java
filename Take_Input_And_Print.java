442704cd741104cc0818e44814b15a4750f4a438
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-01-06 09:41:31 +0530

    import java.util.*; public class Solution{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);        // sc.nextLine();         String s = sc.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       // sc.nextLine();
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
