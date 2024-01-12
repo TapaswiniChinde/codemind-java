91a66ca2c51e5b120ec8a6f19fbf66c7e58501a3
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-01-12 19:34:58 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         double a=r*r*3.14;         System.out.printf("%.2f",a);
    }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double a=r*r*3.14;
+        System.out.printf("%.2f",a);
+    
+}
+}
