ec2aa8ce50a7738a402a06db86e6805ae7b38271
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-01-20 20:06:05 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=(x*y)/(x+y);         System.out.println(z);     } }

---
+++ Water_Taps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=(x*y)/(x+y);
+        System.out.println(z);
+    }
+}
