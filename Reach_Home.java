c7675d7a8c95cdad8a1d5f8860eaa65b5a844fae
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-01-21 13:53:19 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int max=x*5;         if(max>=y)         System.out.println("YES");         else         System.out.println("NO");     } }

---
+++ Reach_Home.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int max=x*5;
+        if(max>=y)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
