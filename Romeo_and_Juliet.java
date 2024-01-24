c38c4e72c9cb31ed97089f9bdcfb840bea01104c
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-01-24 19:29:27 +0530

    import java.util.*; public class Sol{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int ch=(x*5+y*10)/z;         System.out.println(ch);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int ch=(x*5+y*10)/z;
+        System.out.println(ch);
+    } 
+}
