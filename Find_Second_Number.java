0eb57aba910d848aca737a18bb59085e26c0f057
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-02-09 09:49:08 +0530

    import java.util.*; public class Sol{     public static void main(String[] args)     {         Scanner sc= new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int sec=(2*x)-y;         System.out.println(sec);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sol{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int sec=(2*x)-y;
+        System.out.println(sec);
+    }
+}
