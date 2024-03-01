eb5c432206c3e8bbed0239bdc6a8d60081d1af0a
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2024-03-01 10:43:16 +0530

    import java.util.*; public class Vsphere{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);             int a=sc.nextInt();             double r=3.14*a*a*a;             double v=1.333333*r;             System.out.printf("%.2f",v);         } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Vsphere{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            double r=3.14*a*a*a;
+            double v=1.333333*r;
+            System.out.printf("%.2f",v);
+        }
+}
